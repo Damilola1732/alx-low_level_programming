0f52df2c87ae387f57c08aac51bc7058eaa5302d
Bello Damilola <damilolabells@gmail.com>
2022-03-10 07:32:11 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-/** * main - print to the standard output 'Programming is 
-multilingual puzzle' * * Use Puts * Return: retrn 0 */ int 
-main(void) {
-	puts("\"Programming is like building a multilingual 
-	puzzle"); return (0);
+/**
+* main - print to the standard output 'Programming is multilingual puzzle' 
+* 
+* Use Puts 
+* Return: retrn 0 */
+
+
+
+int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
