03aaf0248d7bc5da36283f408dbcecd8d214ecd7
Bello Damilola <damilolabells@gmail.com>
2022-03-11 06:00:49 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdio.h>
 /**
-* main - prints the alphabet in lowercase, followed by a new line. * 
-* You can only use the putchar function (every other function *
- (printf, puts, etc…) is forbidden)
-* All your code should be in the main function
-* You can only use the putchar
+* main - print the last digit of the number stored in the variable n
+*
+*if the last digit of n is greater than 5: the string and is greater than 5
+*if the last digit of n is 0; the string is 0
+*if the last digit of n is less than 6 and not 0: the string
+* and is less than 6 and not 0
 * Return: 0
 */
 
 int main(void)
 {
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-putchar(ch);
-}
-putchar('\n');
-return (0);
+	int n;
+	int last;
+
+	 srand(time(0));
+	 n = rand() - RAND_MAX / 2;
+	last = n % 10;
+	if (last > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	else if (last < 6 && last != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	return (0);
 }
