e0e6b06cdf4f5782260d8813e314d08c57c2d39e
Bello Damilola <damilolabells@gmail.com>
2022-03-11 17:39:30 -0800

    new commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
-/**
-* main - program that prints all possible combinations of single-digit
-*
-* Numbers must be separated by ,followed by a space * Numbers should be printed in ascending order
-* You can only use the putchar function
-* Return: 0
-*/
 
+/**
+* main - Prints numbers between 00 to 89.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-int n = 48;
-int m = 48;
-for (n = 48; n <= 57; n++)
-{
-for (m = 48; m <= 57; m++)
-{
-putchar(n);
-putchar(m);
-if (n != 57 || m != 57)
-{
-putchar(44);
-putchar(32);
-}
-}
-}
-putchar('\n'); return (0);
+int i, e;
+i = 48;
+e = 48;
+	while
+	(e < 58)
+	{
+	i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+			putchar(e);
+			putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
 }
