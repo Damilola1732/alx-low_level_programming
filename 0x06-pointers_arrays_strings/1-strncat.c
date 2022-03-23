51b4dc9d6ab799a2a839ac5aa6276a32eb4e8a33
Bello Damilola <damilolabells@gmail.com>
2022-03-23 20:58:33 +0100

    lil fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,50 +1,32 @@
-#include "holberton.h"
-#include "stdio.h"
-
+#include "main.h"
 /**
-* _strlen - return the length of a string
-*
-*@c: string to be counted
-*Return: Integer
-*/
-
-
-int _strlen(char *c)
-{
-
-int i = 0;
-while (*(c + i) != '\0')
-{
-	i++;
-}
-return (i);
-}
-
-/**
-* _strncat - return the concatenates two strings
-*
-*@dest: destinations string
-*@src: source string
-*@n: number of bytes to append
-* Return: destinations string
-*/
-
+ *_strncat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
+ */
 
 char *_strncat(char *dest, char *src, int n)
-{
 
-int i, len;
-len = _strlen(dest);
+{
+int i, j;
 
 i = 0;
-while (*(src + i) != *(src + n))
+while (dest[i] != '\0')
 {
-	*(dest + len) = *(src + i);
-	i++;
-	len++;
+i++;
 }
-*(dest + len) = '\0';
+
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+
+dest[i] = '\0';
+
 return (dest);
-
-
 }
